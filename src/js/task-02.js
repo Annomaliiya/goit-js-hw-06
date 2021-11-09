2c87fadf3220c39660f1b1b506070d3682f35e0d
Annomaliiya <ivasyukanna@gmail.com>
2021-11-09 16:29:09 +0200

    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const arrayOfIngredients = ingredients.map(ingredient => {
+  const itemIngredient = document.createElement('li');
+  itemIngredient.textContent = ingredient;
+  return itemIngredient;
+});
+
+ingredientsList.append(...arrayOfIngredients);
