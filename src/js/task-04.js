acd81f3b7d08ef1e02fe417a022f24017edb9df0
Annomaliiya <ivasyukanna@gmail.com>
2021-11-09 21:59:48 +0200

    task-4 done

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+const counterValueEl = document.querySelector('#value');
+
+let counterValue = 0;
+
+const minusCountBtn = document.querySelector('[data-action = decrement');
+
+const plusCountBtn = document.querySelector('[data-action = increment');
+
+minusCountBtn.addEventListener('click', () => {
+    counterValue--;
+    counterValueEl.textContent = counterValue;
+});
+
+plusCountBtn.addEventListener('click', () => {
+    counterValue++;
+    counterValueEl.textContent = counterValue;
+});
