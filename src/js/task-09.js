9230d993ef27e35891d8311bdcdf4ff635916d03
Annomaliiya <ivasyukanna@gmail.com>
2021-11-10 14:44:42 +0200

    task-09 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+const btnColorChange = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorName = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+function handleClick() {
+  // body.style.backgroundColor = getRandomHexColor();
+  // colorName.textContent = getRandomHexColor();
+
+  const newColor = body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = newColor;
+}
+
+btnColorChange.addEventListener('click', handleClick);
