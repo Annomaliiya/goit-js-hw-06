294c326a874fbb629324505936c65163c3608c61
Annomaliiya <ivasyukanna@gmail.com>
2021-11-10 14:21:07 +0200

    task-08 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+
+const email = document.querySelector('[type = "email"]');
+
+const password = document.querySelector('[type = "password"]');
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password } } = event.currentTarget;
+
+
+    if (email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены!");
+    }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+
+};
+
+form.addEventListener('submit', onSubmit);
+
 
